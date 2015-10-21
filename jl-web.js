306d21cedbe30426b133749a40324d1287bd0d75
Chris Miller <chrsmllr@gmail.com>
2015-10-21 00:58:00 -0500

    Working lightbox

--- jl-web.js
+++ jl-web.js
@@ -8,45 +8,6 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.Projects.helpers({
-    projects: [
-      {id: 1, img: '/content/img001.jpg'},
-      {id: 2, img: '/content/img002.jpg'},
-      {id: 3, img: '/content/img003.jpg'},
-      {id: 4, img: '/content/img004.jpg'},
-      {id: 5, img: '/content/img005.jpg'},
-      {id: 6, img: '/content/img006.jpg'},
-      {id: 7, img: '/content/img007.jpg'},
-      {id: 8, img: '/content/moto-example.png'},
-      {id: 9, img: '/content/chris-example.png'}
-    ]
-  });
-
-  Template.Projects.onCreated(function() {
-    this.lightbox = new ReactiveVar(false);
-    this.selectedProj = new ReactiveVar({});
-  });
-
-  Template.Projects.events({
-    'click .project__item': function(e, t) {
-      const lightboxInstance = Template.instance().lightbox;
-      const curLightboxVal = lightboxInstance.get();
-
-      lightboxInstance.set(!curLightboxVal);
-      Template.instance().selectedProj.set(this);
-
-      console.log('new lightbox val', lightboxInstance.get());
-      console.log('new selectedProj val', Template.instance().selectedProj.get());
-
-      setTimeout(function() {
-        $(document).one('click', function() {
-          lightboxInstance.set(!lightboxInstance.get());
-          console.log('new lightbox val', lightboxInstance.get());
-        });
-      }, 50);
-    }
-  });
-
   Template.Header.onRendered(function() {
     var application = {
       init: function() {
@@ -78,10 +39,51 @@ if (Meteor.isClient) {
 
     application.init();
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Template.Projects.onCreated(function() {
+    this.lightbox = new ReactiveVar(false);
+    this.selectedProj = new ReactiveVar({});
+  });
+
+  Template.Projects.helpers({
+    projects: [
+      {id: 1, img: '/content/img001.jpg'},
+      {id: 2, img: '/content/img002.jpg'},
+      {id: 3, img: '/content/img003.jpg'},
+      {id: 4, img: '/content/img004.jpg'},
+      {id: 5, img: '/content/img005.jpg'},
+      {id: 6, img: '/content/img006.jpg'},
+      {id: 7, img: '/content/img007.jpg'},
+      {id: 8, img: '/content/moto-example.png'},
+      {id: 9, img: '/content/chris-example.png'}
+    ],
+    lightbox: function() {
+      return Template.instance().lightbox.get();
+    },
+    selectedProj: function() {
+      return Template.instance().selectedProj.get();
+    }
+  });
+
+  Template.Projects.events({
+    'click .project__item': function(e, t) {
+      const lightboxInstance = t.lightbox;
+      const selectedProjInstance = t.selectedProj;
+      const curLightboxVal = lightboxInstance.get();
+
+      lightboxInstance.set(!curLightboxVal);
+      selectedProjInstance.set(this);
+
+      console.log('new lightbox val', lightboxInstance.get());
+      console.log('new selectedProj val', selectedProjInstance.get());
+
+      setTimeout(function() {
+        $(document).one('click', function() {
+          lightboxInstance.set(!lightboxInstance.get());
+          console.log('new lightbox val', lightboxInstance.get());
+        });
+      }, 50);
+    }
   });
 }
 
@@ -100,3 +102,8 @@ Router.route('/contact', function () {
 
   this.render('Contact');
 });
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+  });
+}
