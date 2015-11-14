05828c8cea6a8c4b6f39d95e8c559f82d408aa91
Chris Miller <chris@dialexa.com>
2015-11-14 15:54:24 -0600

    Add 'overview' route with support for projects
    In YAML file, 'projectCategory' can be defined
    to separate photos on the 'overview' route.
    
    Overview route has lightbox for clicking
    individual photos.

--- jl-web.js
+++ jl-web.js
@@ -1,147 +1,17 @@
-if (Meteor.isClient) {
-  Template.Header.helpers({
-    isWork: function() {
-      return Router.current().location.get().path === '/';
-    },
-    isContact: function() {
-      return Router.current().location.get().path === '/contact';
-    }
-  });
-
-  Template.Projects.onCreated(function() {
-    var _this = this;
-    this.lightbox = new ReactiveVar(false);
-    this.selectedProj = new ReactiveVar({});
-    this.currentFeatureImg = new ReactiveVar({});
-
-    return Meteor.call('getClientConfig', function (err, result) {
-      if (result) {
-        Session.set('projects', result.projects);
-      }
-    });
-  });
-
-  Template.Projects.onRendered(function() {
-    var mySVGsToInject = document.querySelectorAll('img.inject-me');
-    SVGInjector(mySVGsToInject);
-  });
-
-  Template.Projects.helpers({
-    lightbox: function() {
-      return Template.instance().lightbox.get();
-    },
-    projects: function() {
-      return Session.get('projects');
-    },
-    selectedProj: function() {
-      return Template.instance().selectedProj.get();
-    },
-    currentFeatureImg: function() {
-      Template.instance().currentFeatureImg.set(Session.get('projects')[0]);
-      return Session.get('projects')[0];
-    },
-    templateGestures: {
-      'swipeleft #feature-img': function(e, t) {
-        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
-      },
-      'swiperight #feature-img': function(e, t) {
-        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'back');
-      },
-    }
-  });
-
-  const navigateFeature = function(ftImg, projects, elem, dir) {
-    const currentFeatureImgInstance = ftImg;
-    const currentFeatureImgPath = ftImg.curValue.img;
-    const curIndex = projects.map(function (proj) {return proj.img}).indexOf(currentFeatureImgPath);
-    let newProj = dir === 'fwd' ? projects[curIndex + 1] : projects[curIndex - 1];
-    let canTransition = false;
-
-    if (newProj) {
-      currentFeatureImgInstance.set(newProj);
-      canTransition = true;
-    } else {
-      newProj = Session.get('projects')[0];
-      currentFeatureImgInstance.set(newProj);
-    }
-
-    const imgElem = elem;
-
-    if (canTransition || dir === 'fwd') {
-      imgElem.fadeOut('fast', function () {
-        imgElem.attr('src', newProj.img);
-        imgElem.fadeIn('fast');
-      });
-    }
-  }
-
-  Template.Projects.events({
-    'click .project__item': function(e, t) {
-      const lightboxInstance = t.lightbox;
-      const selectedProjInstance = t.selectedProj;
-      const curLightboxVal = lightboxInstance.get();
-
-      lightboxInstance.set(!curLightboxVal);
-      selectedProjInstance.set(this);
-
-      setTimeout(function() {
-        $(document).one('click', function() {
-          lightboxInstance.set(!lightboxInstance.get());
-        });
-      }, 50);
-    },
-
-    'click #feature-img': function(e, t) {
-      navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
-    },
-
-    'click #feature-img-fwd': function(e, t) {
-      navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
-    },
-
-    'click #feature-img-back': function(e, t) {
-      navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'back');
-    },
-
-    'keyup': function(e, t) {
-      e.preventDefault();
-      const FWD = 39;
-      const BACK = 37;
-
-      if (e.keyCode === FWD) {
-        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
-      } else if (e.keyCode === BACK) {
-        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'back');
-      }
-    }
-  });
-
-  Template.Contact.helpers({
-    userImg: '/assets/justin.png'
-  });
-
-  const copyClipboard = function(txt) {
-    var textArea = document.createElement("textarea");
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-    textArea.style.opacity = 0;
-
-    textArea.value = txt;
-    document.body.appendChild(textArea);
-    textArea.select();
-
-    document.execCommand('copy');
-  };
-
-  Template.Contact.events({
-    'click #copy-email': function(e, t) {
-      copyClipboard(e.currentTarget.getAttribute('data-email'));
-      setTimeout(function() { Tooltips.hide(); }, 3500);
-    }
+if (Meteor.isServer) {
+  // ---------------------------------------------------------
+  // Server
+  // ---------------------------------------------------------
+  Meteor.startup(function () {
+    YamlConfig.loadFiles(Assets);
   });
 }
 
+
+
+// ---------------------------------------------------------
+// Router
+// ---------------------------------------------------------
 Router.configure({
   layoutTemplate: 'Application'
 });
@@ -164,8 +34,218 @@ Router.route('/overview', function () {
   this.render('Overview');
 });
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    YamlConfig.loadFiles(Assets);
+
+
+
+if (Meteor.isClient) {
+  // ---------------------------------------------------------
+  // Helpers
+  // ---------------------------------------------------------
+  Template.registerHelper("get", function(obj, prop) {
+    return obj[prop];
+  });
+
+
+
+  // ---------------------------------------------------------
+  // Application
+  // ---------------------------------------------------------
+  Template.Application.onCreated(function() {
+    return Meteor.call('getClientConfig', function (err, result) {
+      if (result) {
+        Session.set('projects', result.projects);
+      }
+    });
+  });
+
+
+
+  // ---------------------------------------------------------
+  // Header
+  // ---------------------------------------------------------
+  Template.Header.helpers({
+    isWork: function() {
+      return Router.current().location.get().path === '/';
+    },
+    isContact: function() {
+      return Router.current().location.get().path === '/contact';
+    }
+  });
+
+
+
+  // ---------------------------------------------------------
+  // Projects
+  // ---------------------------------------------------------
+  Template.Projects.onCreated(function() {
+    this.currentFeatureImg = new ReactiveVar({});
+  });
+
+  Template.Projects.onRendered(function() {
+    var mySVGsToInject = document.querySelectorAll('img.inject-me');
+    SVGInjector(mySVGsToInject);
+  });
+
+  Template.Projects.helpers({
+    projects: function() {
+      return Session.get('projects');
+    },
+    currentFeatureImg: function() {
+      Template.instance().currentFeatureImg.set(Session.get('projects')[0]);
+      return Session.get('projects')[0];
+    },
+    templateGestures: {
+      'swipeleft #feature-img': function(e, t) {
+        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
+      },
+      'swiperight #feature-img': function(e, t) {
+        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'back');
+      },
+    }
+  });
+
+  var navigateFeature = function(ftImg, projects, elem, dir) {
+    var currentFeatureImgInstance = ftImg;
+    var currentFeatureImgPath = ftImg.curValue.img;
+    var curIndex = projects.map(function (proj) { return proj.img; }).indexOf(currentFeatureImgPath);
+    var newProj = dir === 'fwd' ? projects[curIndex + 1] : projects[curIndex - 1];
+    var canTransition = false;
+
+    if (newProj) {
+      currentFeatureImgInstance.set(newProj);
+      canTransition = true;
+    } else {
+      newProj = Session.get('projects')[0];
+      currentFeatureImgInstance.set(newProj);
+    }
+
+    var imgElem = elem;
+
+    if (canTransition || dir === 'fwd') {
+      imgElem.fadeOut('fast', function () {
+        imgElem.attr('src', newProj.img);
+        imgElem.fadeIn('fast');
+      });
+    }
+  };
+
+  Template.Projects.events({
+    'click #feature-img': function(e, t) {
+      navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
+    },
+
+    'click #feature-img-fwd': function(e, t) {
+      navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
+    },
+
+    'click #feature-img-back': function(e, t) {
+      navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'back');
+    },
+
+    'keyup': function(e, t) {
+      e.preventDefault();
+      var FWD = 39;
+      var BACK = 37;
+
+      if (e.keyCode === FWD) {
+        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'fwd');
+      } else if (e.keyCode === BACK) {
+        navigateFeature(t.currentFeatureImg, Session.get('projects'), $('#feature-img'), 'back');
+      }
+    }
+  });
+
+
+
+  // ---------------------------------------------------------
+  // Contact
+  // ---------------------------------------------------------
+  Template.Contact.helpers({
+    userImg: '/assets/justin.png'
+  });
+
+  var copyClipboard = function(txt) {
+    var textArea = document.createElement("textarea");
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+    textArea.style.opacity = 0;
+
+    textArea.value = txt;
+    document.body.appendChild(textArea);
+    textArea.select();
+
+    document.execCommand('copy');
+  };
+
+  Template.Contact.events({
+    'click #copy-email': function(e, t) {
+      copyClipboard(e.currentTarget.getAttribute('data-email'));
+      setTimeout(function() { Tooltips.hide(); }, 3500);
+    }
+  });
+
+
+
+  // ---------------------------------------------------------
+  // Overview
+  // ---------------------------------------------------------
+
+  Template.Overview.onCreated(function() {
+    this.lightbox = new ReactiveVar(false);
+    this.selectedProj = new ReactiveVar({});
+  });
+
+  Template.Overview.helpers({
+    lightbox: function() {
+      return Template.instance().lightbox.get();
+    },
+
+    projectCategories: function() {
+      var projects = Session.get('projects');
+      var projectCategories = projects.map(function(proj) {
+        return proj.projectCategory;
+      });
+      return _.uniq(projectCategories);
+    },
+
+    projects: function() {
+      var projects = Session.get('projects');
+      if (projects) {
+        var projectCategories = projects.map(function(proj) {
+          return proj.projectCategory;
+        });
+        var uniqueCategories = _.uniq(projectCategories);
+        return uniqueCategories.reduce(function(hash, cat) {
+          hash[cat] = projects.filter(function(proj) {
+            return proj.projectCategory === cat;
+          });
+
+          return hash;
+        }, {});
+      }
+      return {};
+    },
+
+    selectedProj: function() {
+      return Template.instance().selectedProj.get();
+    }
+  });
+
+  Template.Overview.events({
+    'click .overview__project__item': function(e, t) {
+      var lightboxInstance = t.lightbox;
+      var selectedProjInstance = t.selectedProj;
+      var curLightboxVal = lightboxInstance.get();
+
+      lightboxInstance.set(!curLightboxVal);
+      selectedProjInstance.set(this);
+
+      setTimeout(function() {
+        $(document).one('click', function() {
+          lightboxInstance.set(!lightboxInstance.get());
+        });
+      }, 50);
+    }
   });
 }
