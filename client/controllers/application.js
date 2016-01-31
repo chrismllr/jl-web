5dd2b0172b20e38a9e937dce4b2dd0914ce8b01c
Chris Miller <chrsmllr@gmail.com>
2016-01-31 17:13:50 -0600

    Extra room for safari nav on homepage

--- client/controllers/application.js
+++ client/controllers/application.js
@@ -3,6 +3,11 @@
 // ---------------------------------------------------------
 
 Template.Application.onCreated(function() {
+  var isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+  if (isSafari) {
+    $('body').addClass('safari');
+  }
+
   return Meteor.call('getClientConfig', function (err, result) {
     if (result) {
       Session.set('projects', result.projects);
